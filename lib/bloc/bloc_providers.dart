504a91489e87ccaa1615113c70e5cb7b11157d4e
parodyBit <58690522+parodyBit@users.noreply.github.com>
2021-12-29 17:58:27 -0700

    Refactor bloc_providers
    refactor (BlocProvider) - remove unused import

--- lib/bloc/bloc_providers.dart
+++ lib/bloc/bloc_providers.dart
@@ -3,10 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:witnet_wallet/bloc/theme/theme_bloc.dart';
 import 'package:witnet_wallet/bloc/cache/cache_bloc.dart' as cache;
+import 'package:witnet_wallet/bloc/transactions/value_transfer/create_vtt_bloc.dart';
 import 'package:witnet_wallet/screens/dashboard/dashboard_bloc.dart';
 import 'auth/auth_bloc.dart';
 import '../screens/create_wallet/create_wallet_bloc.dart';
-import 'create_vtt/create_vtt_bloc.dart';
+
 import 'crypto/crypto_bloc.dart';
 import 'explorer/explorer_bloc.dart';
 
