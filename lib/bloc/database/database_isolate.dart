a7286d824331e4a4c6071e0e7a25381367342853
parodyBit <58690522+parodyBit@users.noreply.github.com>
2021-12-29 18:05:17 -0700

    Update database_isolate.dart
    update()- migrate to DbWallet

--- lib/bloc/database/database_isolate.dart
+++ lib/bloc/database/database_isolate.dart
@@ -42,46 +42,69 @@ void dbIsolate(SendPort sendPort) async {
   // listen for messages
   await for (var msg in receivePort) {
     var data = msg[0] as String;
-    SendPort replyToPort = msg[1];
+    SendPort port = msg[1];
     var method = data.split('?')[0];
     var params = json.decode(data.split('?')[1]);
     switch (method) {
       case 'unlockDatabase':
-        var tmp =
-            await dbService.unlockWallet(params['path'], params['password']);
-        if (tmp.runtimeType == DBException) {
-          replyToPort.send(tmp);
-        }
-        replyToPort.send({'unlocked': true});
+        await _unlockDatabase(dbService, port, params);
         break;
       case 'configure':
-        dbService.configure(path: params['path'], password: params['password']);
-        replyToPort.send({'unlocked': true});
+        await _configure(dbService, port, params);
         break;
       case 'readRecord':
-        if (params['type'] == 'String') {
-          var value = await dbService.readString(params['key']);
-          replyToPort.send(value);
-        } else if (params['type'] == 'Map<dynamic, dynamic>') {
-          var value = await dbService.readMap(params['key']);
-          replyToPort.send(value);
-        }
+        await _readRecord(dbService, port, params);
         break;
       case 'getDatabaseService':
-        replyToPort.send({'dbService': dbService});
+        await _getDatabaseService(dbService, port, params);
         break;
       case 'batchRead':
-        var readStack = params['read_ops'] as List<Map<String, Type>>;
-        var results = [];
-        readStack.forEach((op) async {
-          results.add(await dbService.readRecord(op['key']!, op['type']!));
-        });
-        replyToPort.send({'results': results});
+        await _batchRead(dbService, port, params);
         break;
       case 'writeRecord':
-        var result =
-            await dbService.writeRecord(params['key'], params['value']);
-        replyToPort.send({'result': result});
+        await _writeRecord(dbService, port, params);
+        break;
     }
   }
+}
+
+Future<void> _unlockDatabase(DBService dbService, SendPort port, Map<String, dynamic> params) async{
+  var tmp = await dbService.unlockWallet(params['path'], params['password']);
+  if (tmp.runtimeType == DBException) {
+    port.send(tmp);
+  }
+  port.send({'unlocked': true});
+}
+
+Future<void> _configure(DBService dbService, SendPort port, Map<String, dynamic> params) async{
+  dbService.configure(path: params['path'], password: params['password']);
+  port.send({'unlocked': true});
+}
+
+Future<void> _readRecord(DBService dbService, SendPort port, Map<String, dynamic> params) async{
+  if (params['type'] == 'String') {
+    var value = await dbService.readString(params['key']);
+    port.send(value);
+  } else if (params['type'] == 'Map<dynamic, dynamic>') {
+    var value = await dbService.readMap(params['key']);
+    port.send(value);
+  }
+}
+
+Future<void> _getDatabaseService(DBService dbService, SendPort port, Map<String, dynamic> params) async{
+  port.send({'dbService': dbService});
+}
+
+Future<void> _batchRead(DBService dbService, SendPort port, Map<String, dynamic> params) async{
+  var readStack = params['read_ops'] as List<Map<String, Type>>;
+  var results = [];
+  readStack.forEach((op) async {
+    results.add(await dbService.readRecord(op['key']!, op['type']!));
+  });
+  port.send({'results': results});
+}
+
+Future<void> _writeRecord(DBService dbService, SendPort port, Map<String, dynamic> params) async{
+  var result = await dbService.writeRecord(params['key'], params['value']);
+  port.send({'result': result});
 }
