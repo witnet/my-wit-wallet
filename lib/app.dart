357c41b84f8450038efcfbf5fcedae61d58c81fc
parodyBit <58690522+parodyBit@users.noreply.github.com>
2021-12-29 17:54:32 -0700

    Refactor app.dart
    refactor(app) - remove unused imports

--- lib/app.dart
+++ lib/app.dart
@@ -1,17 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:witnet_wallet/screens/create_wallet/create_wallet_bloc.dart';
 import 'package:witnet_wallet/bloc/bloc_providers.dart';
-import 'package:witnet_wallet/bloc/cache/cache_bloc.dart' as cache;
-import 'package:witnet_wallet/bloc/create_vtt/create_vtt_bloc.dart';
 import 'package:witnet_wallet/screens/create_wallet/create_wallet_screen.dart';
 import 'package:witnet_wallet/screens/login/login_screen.dart';
 import 'package:witnet_wallet/screens/test/test_screen.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'bloc/auth/auth_bloc.dart';
-import 'bloc/crypto/crypto_bloc.dart';
-import 'bloc/explorer/explorer_bloc.dart';
+
 import 'bloc/theme/theme_bloc.dart';
 
 class WitnetWalletApp extends StatelessWidget {
