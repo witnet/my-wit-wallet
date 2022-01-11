a9d95354f8eb4dce932e1417e4ab2017efbb3fe1
parodyBit <58690522+parodyBit@users.noreply.github.com>
2022-01-10 17:42:45 -0700

    Update db_wallet.dart
    feature:
     - `generateKey` which uses the `CryptoIsolate`
     - `balanceNanoWit` sums the UTXOs for the external and internal chains.

--- lib/util/storage/database/db_wallet.dart
+++ lib/util/storage/database/db_wallet.dart
@@ -1,7 +1,11 @@
 
 import 'dart:core';
+import 'dart:isolate';
 
 import 'package:witnet/data_structures.dart';
+import 'package:witnet/witnet.dart';
+import 'package:witnet_wallet/bloc/crypto/crypto_isolate.dart';
+import 'package:witnet_wallet/shared/locator.dart';
 import 'package:witnet_wallet/util/witnet/wallet/account.dart';
 import 'package:witnet_wallet/util/witnet/wallet/wallet.dart';
 
@@ -22,11 +26,9 @@ class DbWallet {
   }){
    externalAccounts.forEach((int index, Account account) {
      account.setBalance();
-     balance += account.balance;
    });
    internalAccounts.forEach((int index, Account account) {
      account.setBalance();
-     balance += account.balance;
    });
   }
 
@@ -39,7 +41,7 @@ class DbWallet {
   Map<int, Account> externalAccounts = {};
   Map<int, Account> internalAccounts = {};
   bool nodeAddressActive = false;
-  int balance = 0;
+
   int addressCount = 0;
   Map<String, Utxo> utxoSet = {};
   void addAccount({required Account account, required int index, required KeyType keyType}){
@@ -69,5 +71,45 @@ class DbWallet {
         });
         return _ext;
     }
+  }
+  Future<Account> generateKey(
+      {required int index, KeyType keyType = KeyType.external}) async {
+    ReceivePort response = ReceivePort();
+    // initialize the crypto isolate if not already done so
+
+    await Locator.instance<CryptoIsolate>().init();
+    // send the request
+
+    Locator.instance<CryptoIsolate>().send(
+        method: 'generateKey',
+        params: {
+          'external_keychain': externalXpub,
+          'internal_keychain': internalXpub,
+          'index': index,
+          'keyType': keyType.toString()
+        },
+        port: response.sendPort);
+    var xpub = await response.first.then((value) {
+      return value['xpub'] as Xpub;
+    });
+    switch (keyType) {
+      case KeyType.internal:
+        internalAccounts[index] = Account(address: xpub.address, path: xpub.path!);
+        return internalAccounts[index]!;
+      case KeyType.external:
+        externalAccounts[index] = Account(address: xpub.address, path: xpub.path!);
+        return externalAccounts[index]!;
+    }
+  }
+
+  int balanceNanoWit() {
+    int _balanceNanoWit = 0;
+    internalAccounts.forEach((address, account) {
+      _balanceNanoWit += account.balance;
+    });
+    externalAccounts.forEach((address, account) {
+      _balanceNanoWit += account.balance;
+    });
+    return _balanceNanoWit;
   }
 }
