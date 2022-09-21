71ebf164d8fa4c2154dc06ae3e2683ebe5e6d08f
gabaldon <maria@otherplane.com>
2022-09-21 14:57:45 +0200

    feat(ui): update input theme

---
+++ lib/theme/styles.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class DecorationsLogin {
+  static BoxDecoration borderInput = BoxDecoration(
+    borderRadius: BorderRadius.circular(7.0),
+    border: Border.all(color: Colors.white10, width: 1.0),
+  );
+}
