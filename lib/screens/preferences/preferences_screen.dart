a5b9a9b3b51460e94595d93e3992bf92aa48595a
gabaldon <maria@otherplane.com>
2022-09-23 10:48:48 +0200

    feat(ui): implement reusable switch

--- lib/screens/preferences/preferences_screen.dart
+++ lib/screens/preferences/preferences_screen.dart
@@ -1,40 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:witnet_wallet/widgets/switch.dart';
 import 'package:witnet_wallet/bloc/theme/theme_bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:witnet_wallet/theme/wallet_theme.dart';
 
-class PreferencePage extends StatelessWidget {
-  const PreferencePage({Key? key}) : super(key: key);
+class PreferencePage extends StatefulWidget {
+  PreferencePage({Key? key}) : super(key: key);
 
-  Widget themeWidget(double height) {
-    return Container(
-      height: height,
-      child: ListView.builder(
-        padding: EdgeInsets.all(8),
-        itemCount: WalletTheme.values.length,
-        itemBuilder: (context, index) {
-          // Store the theme for the current ListView item
-          final itemAppTheme = WalletTheme.values[index];
-          return Card(
-            // Style the cards with the to-be-selected theme colors
-            color: walletThemeData[itemAppTheme]!.cardTheme.color,
-            child: ListTile(
-              title: Text(
-                itemAppTheme.toString(),
-                // To show light text with the dark variants...
-                style: walletThemeData[itemAppTheme]!.textTheme.button,
-              ),
-              onTap: () {
-                // This will make the Bloc output a new ThemeState,
-                // which will rebuild the UI because of the BlocBuilder in main.dart
-                BlocProvider.of<ThemeBloc>(context)
-                    .add(ThemeChanged(itemAppTheme));
-              },
-            ),
-          );
-        },
+  @override
+  State<StatefulWidget> createState() => _PreferencePageState();
+}
+
+class _PreferencePageState extends State<PreferencePage> {
+  bool checked = false;
+
+  Widget themeWidget(heigh, context) {
+    return Row(children: [
+      CustomSwitch(
+        checked: checked,
+        primaryLabel: 'Dark Mode',
+        secondaryLabel: 'Light Mode',
+        onChanged: (value) => {
+          setState(() {
+            checked = !checked;
+            BlocProvider.of<ThemeBloc>(context).add(ThemeChanged(checked ? WalletTheme.Dark : WalletTheme.Light));
+          })
+        }
       ),
-    );
+    ]);
   }
 
   @override
@@ -49,7 +42,7 @@ class PreferencePage extends StatelessWidget {
       body: Container(
         child: Column(
           children: [
-            themeWidget(deviceSize.height * 0.25),
+            themeWidget(deviceSize.height * 0.25, context),
           ],
         ),
       ),
