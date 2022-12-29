c7d908099e1d6497736e6851b969c03b51adba82
gabaldon <maria@otherplane.com>
2022-12-29 13:49:45 +0100

    chore(build): delete i10n library

--- lib/app.dart
+++ lib/app.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:witnet_wallet/bloc/bloc_providers.dart';
 import 'package:witnet_wallet/screens/create_wallet/create_wallet_screen.dart';
 import 'package:witnet_wallet/screens/dashboard/view/dashboard_screen.dart';
 import 'package:witnet_wallet/screens/login/view/login_screen.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:witnet_wallet/screens/preferences/preferences_screen.dart';
 import 'package:witnet_wallet/screens/receive_transaction/receive_tx_screen.dart';
 import 'package:witnet_wallet/screens/send_transaction/send_vtt_screen.dart';
@@ -36,18 +34,6 @@ Widget _buildWithTheme(BuildContext context, ThemeState state) {
   return MaterialApp(
     debugShowCheckedModeBanner: false,
     title: 'Material App',
-    localizationsDelegates: [
-      AppLocalizations.delegate,
-      GlobalMaterialLocalizations.delegate,
-      GlobalWidgetsLocalizations.delegate,
-      GlobalCupertinoLocalizations.delegate,
-    ],
-    // usage example: Text(AppLocalizations.of(context)!.helloWorld);
-    supportedLocales: [
-      Locale('en', ''), // English, no country code
-      Locale('es', ''), // Spanish, no country code
-      Locale('ru', ''), // Russian, no country code
-    ],
     home: LoginScreen(),
     theme: state.themeData,
     routes: {
