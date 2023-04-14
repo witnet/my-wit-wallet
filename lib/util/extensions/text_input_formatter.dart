4bd0069f1726ed77a7d38a7fc57d5b7e69fb026d
parodyBit <parody_bit@criptext.com>
2023-04-13 23:27:52 -0600

    fix: vtt validations
    - changed all TextFields to TextFormFields
        - reworked the validators to return String? instead of void. if it is valid it returns null, if it is not valid it returns an error message.
        - moved the state changes to the onChanged controllers in form
    - added `WitValueFormatter`
        Max number of decimals is 9
        Max whole-number is 10
    - added `WitAddressFormatter`
        max length is 42
    
    refactor: remove unused bool
    - added OnTapOutside handlers to the form validation
    
    fix: absolute fee validation

---
+++ lib/util/extensions/text_input_formatter.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/services.dart';
+import 'package:witnet/witnet.dart';
+
+class WitAddressFormatter extends TextInputFormatter {
+  @override
+  TextEditingValue formatEditUpdate(
+      TextEditingValue oldValue, TextEditingValue newValue) {
+    if (newValue.text.isEmpty) {
+      return newValue;
+    }
+    String value = newValue.text;
+
+    if (value.length > 42) {
+      value = value.substring(0, 42);
+    }
+    return TextEditingValue(
+        text: value,
+        selection:
+            TextSelection.fromPosition(TextPosition(offset: value.length)));
+  }
+}
+
+class WitValueFormatter extends TextInputFormatter {
+  @override
+  TextEditingValue formatEditUpdate(
+      TextEditingValue oldValue, TextEditingValue newValue) {
+    if (newValue.text.isEmpty) {
+      return newValue;
+    }
+    String value = newValue.text;
+    if (value.contains('.')) {
+      if (value.startsWith('.')) value = '0$value';
+      String leftPart = value.split('.')[0];
+      String rightPart = value.split('.')[1];
+      leftPart = leftPart.length > 10 ? leftPart.substring(0, 10) : leftPart;
+      rightPart = rightPart.length > 9 ? rightPart.substring(0, 9) : rightPart;
+      value = leftPart + "." + rightPart;
+    } else {
+      if (value.length > 10) {
+        value = value.substring(0, 10);
+      }
+    }
+    return TextEditingValue(
+        text: value,
+        selection:
+            TextSelection.fromPosition(TextPosition(offset: value.length)));
+  }
+}
+
+String? validateWitValue(String? input) {
+  if (input != null) {
+    if (input.isEmpty) return 'This field is required';
+    if (RegExp(r'[a-zA-Z]').hasMatch(input)) return 'Invalid number';
+    if (input.contains('.')) {
+      if (input.split('.').length != 2) return 'Invalid number';
+      if (input.split('.')[1].isEmpty) return 'Invalid number';
+      if (!RegExp(r'^[0-9]+(\.[0-9]{1,9})?$').hasMatch(input))
+        return 'Amount Error: Only 9 decimal digits supported';
+    }
+    if (!RegExp(r'^[0-9]{1,10}(\.[0-9]+)?$').hasMatch(input))
+      return 'Amount too big';
+  }
+  return null;
+}
+
+String? validateAddress(String? input) {
+  if (input != null) {
+    if (input.isEmpty) return 'This field is required';
+    if (RegExp(r'^wit1[02-9ac-hj-np-z]{38}$').hasMatch(input)) {
+      try {
+        Address address = Address.fromAddress(input);
+        assert(address.address.isNotEmpty);
+      } catch (e) {
+        return 'Invalid address';
+      }
+    } else {
+      if (input.length < 42) return 'Address not long enough';
+      return 'Invalid address';
+    }
+  }
+  return null;
+}
