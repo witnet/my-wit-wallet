c25c81c39c10eba9c77b004ddb26a99923689a64
gabaldon <maria@otherplane.com>
2023-08-10 18:46:40 +0200

    fix: add config for e2e testing in ios

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,7 +8,7 @@ import Foundation
 import mobile_scanner
 import path_provider_foundation
 import screen_retriever
-import shared_preferences_macos
+import shared_preferences_foundation
 import url_launcher_macos
 import window_manager
 
