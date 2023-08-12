41257faa63d6ca9d02525176ad04afd1bf17989f
parodyBit <parody_bit@criptext.com>
2023-08-12 12:36:21 -0600

    Fix(Linux): Window Size and Position on launch
    - add linux/window_configuration.cpp` and `.h`
    - move the default window settings to the configuration file.
    - add the `window_configuration.cpp` file to the CMAKE list.

---
+++ linux/window_configuration.cc
@@ -0,0 +1,5 @@
+#include "window_configuration.h"
+
+const char* kFlutterWindowTitle = "myWitWallet";
+const unsigned int kFlutterWindowWidth = 400;
+const unsigned int kFlutterWindowHeight = 700;
---
+++ linux/window_configuration.h
@@ -0,0 +1,8 @@
+#ifndef WINDOW_CONFIGURATION_
+#define WINDOW_CONFIGURATION_
+
+extern const char* kFlutterWindowTitle;
+extern const unsigned int kFlutterWindowWidth;
+extern const unsigned int kFlutterWindowHeight;
+
+#endif  // WINDOW_CONFIGURATION_
