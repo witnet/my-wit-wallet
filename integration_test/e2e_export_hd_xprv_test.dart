7120f5b56734fd0c566c711baabc2361b1ee3bc8
gabaldon <maria@otherplane.com>
2023-11-02 16:23:10 +0100

    feat(e2e-test): implement test for exporting an hd wallet xprv

---
+++ integration_test/e2e_export_hd_xprv_test.dart
@@ -0,0 +1,130 @@
+part of 'test_utils.dart';
+
+Future<void> e2eExportHdXprvTest(WidgetTester tester) async {
+  await initializeTest(tester);
+
+  AppLocalizations _localization =
+      AppLocalizations.of(navigatorKey.currentContext!)!;
+
+  /// Assess what is on the screen
+  walletsExist = isTextOnScreen(_localization.unlockWallet);
+  bool biometricsActive = isTextOnScreen(_localization.cancel);
+
+  /// Cancel the Biometrics popup for linux
+  if (walletsExist && biometricsActive && Platform.isAndroid) {
+    await tapButton(tester, _localization.cancel);
+  }
+  if (walletsExist) {
+    /// Login Screen
+    await enterText(tester, TextFormField, password);
+    await tapButton(tester, _localization.unlockWallet);
+
+    /// Dashboard
+    /// Tap on the first PaddedButton on the screen, which is the identicon
+    /// and brings up the wallet list.
+    await tapButton(tester, PaddedButton, index: 0);
+    await tapButton(tester, FontAwesomeIcons.circlePlus);
+  }
+
+  /// Create or Import Wallet
+  await tapButton(tester, _localization.importWalletLabel);
+  await tapButton(tester, _localization.importXprvLabel);
+
+  /// Wallet Security
+  await scrollUntilVisible(
+      tester, widgetByLabel(_localization.walletSecurityConfirmLabel));
+  await tapButton(tester, LabeledCheckbox);
+  await tapButton(tester, _localization.continueLabel);
+
+  /// Enter Shiekah Compatible Encrypted Xprv
+  await enterText(tester, TextField, sheikahXprv, index: 0);
+  await enterText(tester, TextField, password, index: 1);
+  await tapButton(tester, _localization.continueLabel);
+
+  /// Enter Wallet Name
+  await enterText(tester, TextField, "Test Wallet");
+  await tapButton(tester, _localization.continueLabel);
+
+  /// If the wallet database does not exist we need to enter the password.
+  if (!walletsExist) {
+    await enterText(tester, TextFormField, password, index: 0);
+    await enterText(tester, TextFormField, password, index: 1);
+    await tapButton(tester, _localization.continueLabel);
+  }
+
+  await tester.pumpAndSettle();
+
+  /// Get the currentWallet loaded in the dashboard
+  final DashboardScreenState dashboardScreenState =
+      tester.state(widgetByType(DashboardScreen));
+  Wallet? currentWallet = dashboardScreenState.currentWallet;
+
+  await tapButton(tester, FontAwesomeIcons.gear);
+  await tapButton(tester, _localization.preferenceTabs('wallet'));
+
+  await tapButton(tester, _localization.exportXprv);
+
+  await enterText(tester, TextFormField, password, index: 0);
+
+  await scrollUntilVisible(tester, widgetByText("Verify").first,
+      lastScroll: true);
+
+  await tapButton(tester, "Verify");
+
+  await tester.pumpAndSettle();
+
+  await enterText(tester, TextFormField, password, index: 0);
+  await enterText(tester, TextFormField, password, index: 1);
+
+  await scrollUntilVisible(
+      tester, widgetByText(_localization.generateXprv).first,
+      lastScroll: true);
+
+  await tapButton(tester, _localization.generateXprv);
+
+  // Scroll Save button into view
+  await scrollUntilVisible(
+      tester, widgetByText(_localization.copyXprvLabel).first,
+      lastScroll: true);
+  await tester.pumpAndSettle();
+  await tapButton(tester, _localization.copyXprvLabel);
+
+  // Data from the copied xprv
+  ClipboardData? data = await Clipboard.getData('text/plain');
+
+  /// Dashboard
+  /// Tap on the first PaddedButton on the screen, which is the identicon
+  /// and brings up the wallet list.
+  await tapButton(tester, PaddedButton, index: 0);
+  await tapButton(tester, FontAwesomeIcons.circlePlus);
+
+  /// Create or Import Wallet
+  await tapButton(tester, _localization.importWalletLabel);
+  await tapButton(tester, _localization.importXprvLabel);
+
+  /// Wallet Security
+  await scrollUntilVisible(
+      tester, widgetByLabel(_localization.walletSecurityConfirmLabel));
+  await tapButton(tester, LabeledCheckbox);
+  await tapButton(tester, _localization.continueLabel);
+
+  /// Enter Sheikah Compatible Encrypted Xprv
+  await enterText(tester, TextField, data?.text ?? '', index: 0);
+  await enterText(tester, TextField, password, index: 1);
+  await tapButton(tester, _localization.continueLabel);
+
+  /// Enter Wallet Name
+  await enterText(tester, TextField, "Test Wallet");
+  await tapButton(tester, _localization.continueLabel);
+
+  /// Get the currentWallet loaded in the dashboard
+  final DashboardScreenState dashboardScreenState2 =
+      tester.state(widgetByType(DashboardScreen));
+  Wallet? currentWallet2 = dashboardScreenState2.currentWallet;
+
+  /// Verify the imported wallet and the current address corresponds to the exported xprv
+  expect(currentWallet2!.externalAccounts[0]!.address,
+      currentWallet!.externalAccounts[0]!.address);
+
+  await teardownTest();
+}
--- integration_test/main.dart
+++ integration_test/main.dart
@@ -10,8 +10,9 @@ void main() async {
     testWidgets("Create Mnemonic Test", e2eCreateMnemonicTest);
     testWidgets("Import Mnemonic Test", e2eImportMnemonicTest);
     testWidgets("Import Xprv Test", e2eImportXprvTest);
-    testWidgets("Export Xprv Test", e2eExportXprvTest);
-    testWidgets("Export Xprv Test", e2eSignMessageTest);
+    testWidgets("Export Node Xprv Test", e2eExportXprvTest);
+    testWidgets("Export Hd Xprv Test", e2eExportHdXprvTest);
+    testWidgets("Sign message Test", e2eSignMessageTest);
     testWidgets('Update current wallet', e2eUpdateCurrentWalletTest);
     testWidgets("Auth preferences Test", e2eAuthPreferencesTest);
     testWidgets("Show node stats Test", e2eShowNodeStatsTest);
