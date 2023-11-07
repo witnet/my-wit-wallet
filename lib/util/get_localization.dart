bdaa4dd0555628bda0eb62539df6c7371b606f21
tommytrg <tomastrg22@gmail.com>
2023-11-07 17:27:55 +0100

    fix(test): add i18n support in unit tests
    Add a default locale when no widget context is found, such as when unit
    tests are run.

--- lib/util/get_localization.dart
+++ lib/util/get_localization.dart
@@ -1,5 +1,13 @@
+import 'dart:ui';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:my_wit_wallet/globals.dart';
 
-AppLocalizations get localization =>
-    AppLocalizations.of(navigatorKey.currentContext!)!;
+AppLocalizations get localization {
+  if (navigatorKey.currentContext != null) {
+    return AppLocalizations.of(navigatorKey.currentContext!)!;
+  } else {
+    // Use default localization when no context if found. For instance, when we are running unit
+    // tests and no Widget is being rendered
+    return lookupAppLocalizations(Locale('en'));
+  }
+}
