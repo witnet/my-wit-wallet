9c8133ef553b6b639b17b82e69c7e08df30913bb
tommytrg <tomastrg22@gmail.com>
2024-08-06 17:53:44 +0200

    screenshot

--- integration_test/test_utils.dart
+++ integration_test/test_utils.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 import 'dart:typed_data';
 import 'dart:ui' as ui;
-
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
