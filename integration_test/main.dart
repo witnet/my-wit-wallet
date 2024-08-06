c79c60b2d66427e669927d09546ca89ee0f2ee90
tommytrg <tomastrg22@gmail.com>
2024-08-06 18:15:31 +0200

    screenshot

--- integration_test/main.dart
+++ integration_test/main.dart
@@ -7,19 +7,17 @@ void main() async {
   globals.testingActive = true;
 
   group("End To End Mnemonic Tests", () {
-    testWidgets("Create Mnemonic Test", e2eCreateMnemonicTest, skip: true);
-    testWidgets("Import Mnemonic Test", e2eImportMnemonicTest, skip: true);
-    testWidgets("Import Xprv Test", e2eImportXprvTest, skip: true);
-    testWidgets("Export Node Xprv Test", e2eExportXprvTest, skip: true);
+    testWidgets("Create Mnemonic Test", e2eCreateMnemonicTest);
+    testWidgets("Import Mnemonic Test", e2eImportMnemonicTest);
+    testWidgets("Import Xprv Test", e2eImportXprvTest);
+    testWidgets("Export Node Xprv Test", e2eExportXprvTest);
     testWidgets("Export Hd Xprv Test", e2eExportHdXprvTest);
-    testWidgets("Sign message Test", e2eSignMessageTest, skip: true);
-    testWidgets('Update current wallet', e2eUpdateCurrentWalletTest,
-        skip: true);
-    testWidgets("Auth preferences Test", e2eAuthPreferencesTest, skip: true);
-    testWidgets("Show node stats Test", e2eShowNodeStatsTest, skip: true);
-    testWidgets("Re-establish wallets", e2eReEstablishWallets, skip: true);
-    testWidgets("Import Xprv From Sheikah Test", e2eImportXprvFromSheikahTest,
-        skip: true);
-    testWidgets("Update theme color", e2eUpdateThemeColorTest, skip: true);
+    testWidgets("Sign message Test", e2eSignMessageTest);
+    testWidgets('Update current wallet', e2eUpdateCurrentWalletTest);
+    testWidgets("Auth preferences Test", e2eAuthPreferencesTest);
+    testWidgets("Show node stats Test", e2eShowNodeStatsTest);
+    testWidgets("Re-establish wallets", e2eReEstablishWallets);
+    testWidgets("Import Xprv From Sheikah Test", e2eImportXprvFromSheikahTest);
+    testWidgets("Update theme color", e2eUpdateThemeColorTest);
   });
 }
